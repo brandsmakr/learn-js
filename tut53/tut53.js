44e4159af2586339a4acf671f183641e0bcfe459
Talha <brandsmakr8@gmail.com>
2022-06-11 14:15:21 -0700

    loops, array, prompt, confirm, for each, while, do while, for in, for of loops injs

---
+++ tut53/tut53.js
@@ -0,0 +1,30 @@
+let name = "talha";
+let name1 = "fasih";
+let name3 = "Adbul Sami";
+let name4 = "Sabaq bahti";
+
+// console.log(name + " is a good boy");
+// console.log(name1 + " is a good boy");
+// console.log(name3 + " is a good boy");
+// console.log(name4 + " is a good boy");
+
+let greetText = "Good Morning";
+function greet(name, greetText)
+{
+    console.log(greetText + " " + name);
+    console.log(name + " is a good boy");
+}
+
+// greet(name, greetText);
+// greet(name1, greetText);
+// greet(name3, greetText);
+// greet(name4, greetText);
+
+function sum(a,b,c)
+{
+    let d = a + b + c ;
+    return d;
+}
+ 
+let greetText1 = sum(2, 3, 4, 6);
+greet(name, greetText1)
